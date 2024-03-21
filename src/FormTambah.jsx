520fddd7a23a4b079d09bce6241979ab208dee07
Annas975 <abu227088@student.smkn1kandeman.sch.id>
2024-03-21 10:26:35 +0700

    penambahan NoteContext.jsx

--- src/FormTambah.jsx
+++ src/FormTambah.jsx
@@ -6,7 +6,7 @@ function FormTambah({ onAdd }) {
     const [title, setTitle] = useState("")
     const [note, setNotes] = useState("")
 
-    const handlerSubsmit = () => {
+    const handlerSubmit = () => {
         onAdd(title, note);
         setTitle("")
         setNotes("")
@@ -15,19 +15,40 @@ function FormTambah({ onAdd }) {
     return (
         <>
             <div className="mt-1">
+         
+            {/* <textarea
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                    className="input w-full rounded-sm p-1 text-sm"
+                    type="text"
+                    name="title"
+                    id=""
+                    placeholder='title' >
+                </textarea>
+                <textarea
+                    value={note}
+                    onChange={e => setNotes(e.target.value)}
+                    type="text"
+                    name=""
+                    id=""
+                    placeholder="tulis mengenai pengalaman kamu dengan tailwind"
+                    className="input w-full rounded-sm p-1 text-sm mt-1">   
+                </textarea> */}
+
                 <textarea
                     value={title}
                     onChange={e => setTitle(e.target.value)}
                     className="input w-full rounded-sm p-1 text-sm"
                     type="text"
                     name="title"
+                    id=""
                     placeholder='title'
                 >
                 </textarea>
 
                 <textarea
                     value={note}
-                    onChange={e => setTitle(e.target.value)}
+                    onChange={e => setNotes(e.target.value)}
                     className="input w-full rounded-sm p-1 text-sm mt-1"
                     type="text"
                     name="note"
@@ -37,9 +58,9 @@ function FormTambah({ onAdd }) {
 
             </div>
             <button
-                onClick={() => handlerSubsmit()}
-                className='bg-white pt-1 pb-1 pl-2 pr-2 rounded-lg shadow-sm hover:shadow-lg hover:bg-slate-100 hover:bg-contain'
-            >Add Note
+                onClick={() => handlerSubmit()}
+                className='bg-white pt-1 pb-1 pl-2 pr-2 rounded-lg shadow-sm hover:shadow-lg hover:bg-slate-100 hover:bg-contain'>
+                    Add Note
             </button>
         </>
     )
